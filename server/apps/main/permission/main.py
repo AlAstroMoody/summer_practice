8dad5923174ae28a58c90b2c3269894c1bec6f7b
Alex <aastrotenko@mail.ru>
2020-09-04 11:09:43 +0700

    исправил права доступа

--- server/apps/main/permission/main.py
+++ server/apps/main/permission/main.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+from apps.places.models import Place
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
@@ -11,9 +13,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
 
 class IsOwnerPlaceWithDishOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        place = Place.objects.get(pk=request.data['place'])
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated and
+            place.owner == request.user
+        )
 
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if obj.place.owner == request.user:
-            return True
